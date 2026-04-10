bdbee031d9309422857df100e579eb5032201c15
Abigael Ongwae <abbykerry17@gmail.com>
2026-04-10 23:59:01 +0300

    added another solution

--- palindromeCheck.js
+++ palindromeCheck.js
@@ -11,3 +11,23 @@ function isPalindrome(str) {
 console.log(isPalindrome("madam"));   // true
 console.log(isPalindrome("hello"));   // false
 console.log(isPalindrome("Racecar")); // true
+
+//solution using two pointer
+function isPalindromeTwoPointer(str){
+    let left = 0; //start of string
+    let right = str.length -1; //last index of string
+
+    //then we loop through the string and check if the characters at the left and right pointers are the same, ignoring case and non-alphanumeric characters
+    while(left<right){
+        if (str[left].toLowerCase() !== str[right].toLowerCase()
+        ){
+            return false;
+        }
+        left++; //move from left to right
+        right--;//move from right to left
+    }
+    return true;
+}
+
+console.log(isPaldindromeTwoPointer("Abby"));//false
+console.log(isPaldindromeTwoPointer())
