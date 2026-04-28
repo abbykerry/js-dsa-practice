7fe92b78a94e99d117e13d1af6b061fbb73f5e6e
Abigael Ongwae <abbykerry17@gmail.com>
2026-04-28 23:25:52 +0300

    solution 2 using hash map method

--- medium-style.js
+++ medium-style.js
@@ -19,4 +19,27 @@ function twoSum(nums, target) {
     
 }
 // nums = [2, 7, 11, 15]
-console.log(twoSum([2, 7, 11, 15], 9));
+console.log("The indices of the two numbers are: ", twoSum([2, 7, 11, 15], 9));
+// time complexity for solution 1 is O(n^2)
+
+
+//solution 2 
+function twoNumbers(nums, target){
+    let myStorage = {}; //object to store numbers and their indices eg 1:2
+    for(let i = 0; i < nums.length; i++){
+        let otherNo = target - nums[i];
+        if(myStorage[otherNo] !== undefined){
+            return [myStorage[otherNo], i];
+        }
+        myStorage[nums[i]] = i; //storing the no and its index
+        console.log(myStorage);
+    }
+}
+
+console.log("The indices of the two numbers are:", twoNumbers([3,5,7,6,4,2], 10));
+
+//time complexity
+//myStorage = {} is instant lookup (O(1)) and
+//storing is instant (O(1))
+//total time = (work per step) * (number of steps). but loop runs n times
+//so n * O(1) = O(n)
