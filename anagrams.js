88c9c4e9a7d93525b5c2880f870d3cf636b30458
Abigael Ongwae <abbykerry17@gmail.com>
2026-05-07 23:49:56 +0300

    solution 1 using sorting

--- anagrams.js
+++ anagrams.js
@@ -24,3 +24,20 @@ Output:
 ]
 // 📌 Solution
 
+//solution1 - using sorting
+
+function groupAnagrams(strs) {
+    let map = {};
+
+    for (let word of strs) {
+        let sorted = word.split("").sort().join("");
+
+        if (!map[sorted]) {
+            map[sorted] = [];
+        }
+
+        map[sorted].push(word);
+    }
+
+    return Object.values(map);
+}
